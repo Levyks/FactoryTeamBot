89f34fb10309a70caf8fbd4df9c609602b5c150f
Levy Barbosa <marcio.levy20@gmail.com>
2022-12-23 16:26:51 -0300

    changed port, added health controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using FactoryTeamBot.Dtos;
+using FactoryTeamBot.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactoryTeamBot.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return Ok(new
+        {
+            message = "I'm alive!"
+        });
+    }
+    
+}
